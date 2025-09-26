4e98e0e27d2257057dab19e0a5b3cbcfe3ddcd25
CollinWilkerson <wilkerc2@my.erau.edu>
2025-09-26 12:42:42 -0700

    finish decorator and visitor

---
+++ Assets/Chapter10/Implementing Power-ups with the Visitor pattern/Scripts/ClientVisitor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Pattern.Visitor
+{
+    public class ClientVisitor : MonoBehaviour
+    {
+        //these are the scriptable objects
+        public PowerUp enginePowerUp;
+        public PowerUp shieldPowerUp;
+        public PowerUp weaponPowerUp;
+        public PowerUp superPowerUp;
+        
+        private BikeController _bikeController;
+
+        void Start()
+        {
+            _bikeController = 
+                gameObject.
+                    AddComponent<BikeController>();
+        }
+
+        //each accept technically visits each thing, but only has an effect on certain parts of the bike
+        void OnGUI()
+        {
+            if (GUILayout.Button("PowerUp Shield"))
+                _bikeController.Accept(shieldPowerUp);
+
+            if (GUILayout.Button("PowerUp Engine"))
+                _bikeController.Accept(enginePowerUp);
+            
+            if (GUILayout.Button("PowerUp Weapon"))
+                _bikeController.Accept(weaponPowerUp);
+
+            if (GUILayout.Button("PowerUp All"))
+                _bikeController.Accept(superPowerUp);
+        }
+    }
+}
